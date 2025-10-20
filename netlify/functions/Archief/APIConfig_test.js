9701d4a217375c803ccb0ce2d365d014fe7ff879
AtalianQR <atalianqr@gmail.com>
2025-10-20 08:24:50 +0200

    verwijder oude

--- netlify/functions/Archief/APIConfig_test.js
+++
@@ -1,8 +0,0 @@
-// APIConfig_test.js
-
-export const API_KEY = "03F5BDB822224699AD5077BE481BB627";
-export const BASE_URL = "https://atalian-test.ultimo.net/api/v1";
-
-// Gebruik duidelijke namen voor ApplicationElementId's:
-export const APP_ELEMENT_QueryAtalianJobs = "6379d9e0a70545a6d90679e46e6ab715";
-export const APP_ELEMENT_OneAtalianJob = "3f92bbfca30445ff875f3a9d956441be";
